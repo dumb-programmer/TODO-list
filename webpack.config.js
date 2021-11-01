5da0aa12e20d543eadedea776e19c0055b1fd055
dumb-programmer <asadkhan6192@gmail.com>
2021-11-01 09:32:51 +0500

    Add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -24,4 +24,12 @@ module.exports = {
             },
         ],
     },
+    mode: 'development',
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 9000,
+    },
 }
